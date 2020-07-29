054e9183532dd70b7dc95ca08e7a99a6b5f650e2
Mina Amir <eng.mina23@gmail.com>
2020-07-29 13:02:44 +0200

    add Fruitcake Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,31 +8,31 @@ class Cors
 {
 
 
-    public function handle($request, Closure $next)
-    {
+//    public function handle($request, Closure $next)
+//    {
 
 //        header("Access-Control-Allow-Origin: *");
 
 
         //header('Access-Control-Allow-Origin:  *');
-        header('Access-Control-Allow-Origin:  *');
-        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-        header('Access-Control-Allow-Methods:  POST, PUT');
-        // ALLOW OPTIONS METHOD
-        $headers = [
-            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
-        ];
-        if($request->getMethod() == "OPTIONS") {
-            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
-            return Response::make('OK', 200, $headers);
-        }
-
-        $response = $next($request);
-        foreach($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
-    }
+//        header('Access-Control-Allow-Origin:  *');
+//        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+//        header('Access-Control-Allow-Methods:  POST, PUT');
+//        // ALLOW OPTIONS METHOD
+//        $headers = [
+//            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+//            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
+//        ];
+//        if($request->getMethod() == "OPTIONS") {
+//            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+//            return Response::make('OK', 200, $headers);
+//        }
+//
+//        $response = $next($request);
+//        foreach($headers as $key => $value)
+//            $response->header($key, $value);
+//        return $response;
+//    }
 
     /**
      * Handle an incoming request.
@@ -41,12 +41,13 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-//    public function handle($request, Closure $next)
-//    {
-//        return $next($request)
-//            ->header('Access-Control-Allow-Origin', '*')
-//            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//    }
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Accept,Content-Type, X-Auth-Token, Authorization, Origin');
+    }
 //    public function handle($request, Closure $next)
 //    {
 //        return $next($request);
