71e877d747d860fb00aba721e9fccdcedd86a7c7
Mina Amir <eng.mina23@gmail.com>
2020-07-29 13:14:03 +0200

    add Fruitcake Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -46,7 +46,7 @@ class Cors
         $headers = [
             'Access-Control-Allow-Origin'=> '*',
             'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'=> 'Accept,Content-Type, X-Auth-Token, Origin'
+            'Access-Control-Allow-Headers'=> 'Accept,Content-Type, X-Auth-Token, Authorization, Origin'
         ];
         if($request->getMethod() == "OPTIONS") {
             // The client-side application can set only headers allowed in Access-Control-Allow-Headers
